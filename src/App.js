d6b4cccc885cd0e58b10224cda2a74e35220ee33
Anna Rachel Valdez <annarachel.valdez@gmail.com>
2020-12-21 10:50:53 -0800

    modern-react widgets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+const items = [
+	{
+		title: 'What is React?',
+		content: 'Front end JS Framework',
+	},
+	{
+		title: 'Why use React?',
+		content: 'Fave Front end JS Framework',
+	},
+	{
+		title: 'How do you use?',
+		content: 'Use React by creating components ',
+	},
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const options = [
+	{
+		label: 'The Color Red',
+		value: 'red',
+	},
+	{
+		label: 'The Color Blue',
+		value: 'blue',
+	},
+	{
+		label: 'The Color Green',
+		value: 'green',
+	},
+];
+export default () => {
+	return (
+		<div>
+			<Translate />
+		</div>
+	);
+};
